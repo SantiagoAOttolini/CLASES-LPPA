918420d50255ba24ff83e9ef438616f82d35e2a4
santiago <santiago.ottolini98@gmail.com>
2019-06-05 13:55:14 -0300

    Agregado material de alumnos v1

---
+++ PRACTICA MATERIAL ALUMNOS/VARIABLES-JS/variables.js
@@ -0,0 +1,20 @@
+//1
+var numero1 = 1;
+var numero2 = 2;
+alert(numero1 + numero2);
+alert(numero1-numero2);
+alert(numero1*numero2);
+alert(numero1/numero2);
+alert(numero1++);
+alert(numero2++);
+alert(numero1--);
+alert(numero2--);
+console.log(alert);
+
+//3
+var numero3 = 4536;
+var numero4=365;
+parseInt("Tengo" + numero3/numero4 + "anos");
+
+//4
+
